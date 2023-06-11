3d28b8db5cbd586cce182696a6887f6d938f1707
bcjwalker <bwal9030@gmail.com>
2023-06-11 18:01:00 +1000

    html + css almost complete; beginnings of projects code

--- public/script.js
+++ public/script.js
@@ -12,24 +12,14 @@
 ###############
 */
 
-///
+// Get project DB
+const projectData = require("./assets/projects-db.json");
+const projectDataNo = projectData.length;
+console.log(projectData);
+
 // DOM
   // Get body
   const pageBody = document.body;
-  // Get header 
-  const pageHeader = document.querySelector('header');
-
-  // Aside
-  const asideExpBtnContainer = document.querySelector('#aside-infobox-btn-container');
-  const asideExp = document.querySelector('#aside-infobox-content');
-  const asideExpBtn = document.querySelector('#aside-infobox-btn');
-
-  // Nav
-  const nav = document.querySelector('nav');
-  const navListBtns = document.querySelectorAll('.navbox-btn');
-  const navListBtnPs = document.querySelectorAll('.navbox-btn-p');
-  const navHbBtn = document.querySelector('#head-hamburger-btn');
-///
 
 
 /*
@@ -41,17 +31,41 @@
 // Called when page has been completely loaded
 window.addEventListener('load', function() {
     console.log('Page loaded, starting postload scripts~')
+
+    // Init website functionality basics
     initNav();
-    initHeaderScrolling();
+    initHead();
     initAside();
-    //initProjects();
+
+    // Finally, init projects functionality
+    initProjects();
 });
 
-// Init:
-//  Show header on scroll down past signature
-//  Hide header on scroll up past signature
-function initHeaderScrolling() {
-    document.body.addEventListener('scroll', () => {
+// Init nav's context menu button
+function initNav() {
+    // DOM
+    const nav = document.querySelector('nav');
+    const navListBtns = document.querySelectorAll('.navbox-btn');
+    const navListBtnPs = document.querySelectorAll('.navbox-btn-p');
+    const navHbBtn = document.querySelector('#head-hamburger-btn');
+
+    navHbBtn.addEventListener('click', () => {
+        // Shrink actual buttons so overlay thing is lessened
+        // Remove nav labels
+        navListBtns.forEach((element) => {
+            element.classList.toggle('closed');
+        });
+        // Shrink nav size in body grid
+        pageBody.classList.toggle('retract');
+    });
+}
+
+// Init header that appears/vanishes based on scroll pos (before/after signature image)
+function initHead() {
+    // DOM
+    const pageHeader = document.querySelector('header');
+
+      document.body.addEventListener('scroll', () => {
         // Grab our scroll Y coords
         const currentScroll = document.body.scrollTop;
         
@@ -61,33 +75,51 @@ function initHeaderScrolling() {
         // If we're higher than the signature, hide header 
         pageHeader.classList.toggle('fadeout', currentScroll < 220);
     });
-};
+}
 
 // Init aside's expand/collapse button
 function initAside() {
+    // DOM
+    const asideExpBtnContainer = document.querySelector('#aside-infobox-btn-container');
+    const asideExp = document.querySelector('#aside-infobox-content');
+    const asideExpBtn = document.querySelector('#aside-infobox-btn');
+
     asideExpBtnContainer.addEventListener('click', () => {
         asideExp.classList.toggle('closed');
         asideExpBtn.classList.toggle('closed');
     });
-    
-};
+}
 
-// Init nav's context menu button
-function initNav() {
-    navHbBtn.addEventListener('click', () => {
-        // Remove nav labels
-        navListBtnPs.forEach((element) => {
-            element.classList.toggle('closed');
-        })
-        // Shrink actual buttons so overlay thing is lessened
-        navListBtns.forEach((element) => {
-            element.classList.toggle('closed');
-        })
-        // Shrink nav size in body grid
-        pageBody.classList.toggle('retract');
-    });
-};
+////
+// Projects:
+//   All code required for the projects section to work
+////
 
-function initProjects() {
-    
-};
+// Init project section
+function initProjects() { 
+
+    // Render for all entries
+    for(i=0; i < projectDataNo; i++) {
+        renderProject(i);
+    };
+}
+
+// Populate projects section with projects from DB
+function renderProject(pos) {
+    // Grab project thumb carousel and template
+    const projectThumbCardousel = document.querySelector('#projects-big-cardousel');
+    const projectThumbTemplate = document.querySelector('template.projects-big-card-template');
+
+        // Grab details
+        const projectThumbPrefab = projectThumbTemplate.content.cloneNode(true);
+        const projectTPhead = projectThumbPrefab.querySelector('.projects-big-card-details-h3')
+        const projectTPtags = projectThumbPrefab.querySelector('.projects-big-card-details-p')
+        console.log(projectThumbPrefab);
+
+    // Fill new project metadata
+    projectTPhead.textContent = projectData[pos].name;
+    projectTPtags.textContent = projectData[pos].tags;
+
+    // Append to cardousel
+    projectThumbCardousel.appendChild(projectThumbPrefab);
+}
